ef0412524e4278efc98f40c537cd9b3dd59a9fbe
OmarPython <130500721+Omarpython@users.noreply.github.com>
2023-05-04 22:50:46 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(
+    name= 'Style-Terminal',
+    version= '0.0.1',
+    author= 'Omar-Moahmed',
+    description="A library that deals with the device, device style, adding commands, and dealing with colors",
+    classifiers=[
+    "Progtamming Language :: Python :: 3",
+    "Operating System :: MacOS::MacOS X",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
